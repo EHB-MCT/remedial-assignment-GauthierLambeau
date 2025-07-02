41bc05c8c8c3b74290ea7f8288028129f5052d34
GauthierLambeau <114060286+GauthierLambeau@users.noreply.github.com>
2025-07-02 14:41:41 +0200

    feat: added script om gebouwen te kunnen plaatsen + UI buttons
    buildingplacer: laat de speler gebouwen plaatsen op een plane om geld dte kunnen genereren
    
    UI buttons: laaat de speler het type gebouw selecteren

--- Assets/Code/Scripts/BuildingPlacer.cs
+++ Assets/Code/Scripts/BuildingPlacer.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingPlacer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject[] buildingPrefabs; // Liste des prefabs de bâtiments
+    public int[] buildingCosts; // Coûts associés aux bâtiments
+    private int selectedBuildingIndex = 0;
 
-    // Update is called once per frame
+    public LayerMask placementLayer; // Assure-toi que le sol est dans ce Layer
+
     void Update()
     {
-        
+        // Empêche le placement si on clique sur un élément UI
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceBuilding();
+        }
+    }
+
+    public void SelectBuilding(int index)
+    {
+        if (index >= 0 && index < buildingPrefabs.Length)
+        {
+            selectedBuildingIndex = index;
+        }
+    }
+
+    void PlaceBuilding()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, placementLayer))
+        {
+            int cost = buildingCosts[selectedBuildingIndex];
+            if (MoneyManager.Instance.SpendMoney(cost))
+            {
+                Instantiate(buildingPrefabs[selectedBuildingIndex], hit.point, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Pas assez d'argent pour placer ce bâtiment.");
+            }
+        }
     }
 }
