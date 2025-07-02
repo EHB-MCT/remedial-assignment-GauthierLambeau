fc12d81af778ec60c2f9ff9ad0aeb44ae319941c
GauthierLambeau <114060286+GauthierLambeau@users.noreply.github.com>
2025-07-02 17:20:18 +0200

    feat: added basic logica voor de upgrades

--- Assets/Code/Scripts/UpgradeManager.cs
+++ Assets/Code/Scripts/UpgradeManager.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class UpgradeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public class Upgrade
     {
-        
+        public string upgradeName;
+        public int cost;
+        public float multiplier = 1.5f;
+        public Button upgradeButton;
+        public bool purchased = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Upgrade> upgrades;
+    public List<Building> allBuildings; 
+
+    private void Start()
     {
-        
+        foreach (Upgrade upgrade in upgrades)
+        {
+            upgrade.upgradeButton.onClick.AddListener(() => ApplyUpgrade(upgrade));
+        }
+    }
+
+    void ApplyUpgrade(Upgrade upgrade)
+    {
+        if (upgrade.purchased) return;
+
+        if (MoneyManager.Instance.SpendMoney(upgrade.cost))
+        {
+            foreach (Building building in allBuildings)
+            {
+                building.moneyPerSecond = Mathf.RoundToInt(building.moneyPerSecond * upgrade.multiplier);
+            }
+
+            upgrade.purchased = true;
+            upgrade.upgradeButton.interactable = false;
+        }
     }
 }
